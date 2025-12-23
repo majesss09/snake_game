db10adf4282b9fb3f69f9ac7295bb3d4616c4697
majesss <lukimajewski123@gmail.com>
2025-12-23 20:06:09 +0100

    Finished the snake game.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -9,6 +11,8 @@ screen.tracer(0)
 screen.title("Snake")
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -20,8 +24,20 @@ game_over = False
 while not game_over:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
 
+    if snake.head.distance(food) < 15:
+        food.respawn()
+        snake.extend()
+        scoreboard.add_point()
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        scoreboard.game_over()
+        game_over = True
+
+    for segment in snake.snake[1:]:
+        if snake.head.distance(segment) < 10:
+            scoreboard.game_over()
+            game_over = True
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -5,17 +5,24 @@ UP = 90
 DOWN = 270
 LEFT = 180
 RIGHT = 0
+STARTING_POSITIONS = [(0,0),(-20,0),(-40,0)]
 class Snake():
     
     def __init__(self):
         self.snake = []
-        for i in range(3):
-            square = Turtle(shape="square",undobuffersize=20)
-            square.color("white")
-            square.penup()
-            square.goto(x=(-MOVE_DISTANCE)*i,y=0)
-            self.snake.append(square)
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
         self.head = self.snake[0]
+
+    def add_segment(self, position):
+        square = Turtle(shape="square",undobuffersize=20)
+        square.color("white")
+        square.penup()
+        square.goto(position)
+        self.snake.append(square)
+
+    def extend(self):
+        self.add_segment(self.snake[-1].position())
     
     def move(self):
         for seg_num in range(len(self.snake) - 1, 0, -1):
